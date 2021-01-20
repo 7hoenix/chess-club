56845beb9a4acec4af6bfb6d8a47ecaa7f923215
7hoenix <jpholzmann@gmail.com>
2021-01-20 09:16:47 -0600

    feat: Add generated UserManager context and User.

---
+++ test/chess_club/user_manager_test.exs
@@ -0,0 +1,66 @@
+defmodule ChessClub.UserManagerTest do
+  use ChessClub.DataCase
+
+  alias ChessClub.UserManager
+
+  describe "users" do
+    alias ChessClub.UserManager.User
+
+    @valid_attrs %{password: "some password", username: "some username"}
+    @update_attrs %{password: "some updated password", username: "some updated username"}
+    @invalid_attrs %{password: nil, username: nil}
+
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> UserManager.create_user()
+
+      user
+    end
+
+    test "list_users/0 returns all users" do
+      user = user_fixture()
+      assert UserManager.list_users() == [user]
+    end
+
+    test "get_user!/1 returns the user with given id" do
+      user = user_fixture()
+      assert UserManager.get_user!(user.id) == user
+    end
+
+    test "create_user/1 with valid data creates a user" do
+      assert {:ok, %User{} = user} = UserManager.create_user(@valid_attrs)
+      assert user.password == "some password"
+      assert user.username == "some username"
+    end
+
+    test "create_user/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = UserManager.create_user(@invalid_attrs)
+    end
+
+    test "update_user/2 with valid data updates the user" do
+      user = user_fixture()
+      assert {:ok, %User{} = user} = UserManager.update_user(user, @update_attrs)
+      assert user.password == "some updated password"
+      assert user.username == "some updated username"
+    end
+
+    test "update_user/2 with invalid data returns error changeset" do
+      user = user_fixture()
+      assert {:error, %Ecto.Changeset{}} = UserManager.update_user(user, @invalid_attrs)
+      assert user == UserManager.get_user!(user.id)
+    end
+
+    test "delete_user/1 deletes the user" do
+      user = user_fixture()
+      assert {:ok, %User{}} = UserManager.delete_user(user)
+      assert_raise Ecto.NoResultsError, fn -> UserManager.get_user!(user.id) end
+    end
+
+    test "change_user/1 returns a user changeset" do
+      user = user_fixture()
+      assert %Ecto.Changeset{} = UserManager.change_user(user)
+    end
+  end
+end
